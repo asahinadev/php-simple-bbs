974c6c4682d258ff88e81da0b83648d1e2b33dc0
asahinadev <dev.account.01@mail.mirageworld.jp>
2020-05-03 19:26:40 +0900

    env

--- html/dbconnect.php
+++ html/dbconnect.php
@@ -1,6 +1,11 @@
 <?php
 try {
-    $db = new PDO('mysql:dbname=bbsuser;host=db;charset=utf8', 'bbsuser', 'bbsuser');
+	$dbuser = isset($_ENV["MYSQL_USERNAME"]) ? $_ENV["MYSQL_USERNAME"] : "bbsuser";
+	$dbpass = isset($_ENV["MYSQL_PASSWORD"]) ? $_ENV["MYSQL_PASSWORD"] : "bbsuser";
+	$dbname = isset($_ENV["MYSQL_USERNAME"]) ? $_ENV["MYSQL_DATABASE"] : "bbsuser";
+	$dbhost = isset($_ENV["MYSQL_SERVER"])   ? $_ENV["MYSQL_SERVER"]   : "db";
+
+    $db = new PDO('mysql:dbname='.$dbname .';host='.$dbhost .';charset=utf8', $dbuser, $dbpass);
 } catch (PDOException $e) {
     echo 'DB接続エラー： ' . $e->getMessage();
 }
